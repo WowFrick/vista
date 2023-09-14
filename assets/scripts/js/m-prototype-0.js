33148bcbbc782544659108719fd036722291d6a5
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-09-14 13:31:23 +0200

    Movies

--- assets/scripts/js/m-prototype-0.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("M").className += " active";
-document.getElementById("M-Prototype").className += " active";
-document.getElementById("M-Prototype-0").className += " active";
--- assets/scripts/js/t-prototype-0.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("T").className += " active";
-document.getElementById("T-Prototype").className += " active";
-document.getElementById("T-Prototype-0").className += " active";
