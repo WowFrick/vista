afc0d62697a38bc43e070cde6f41292332e0e3cb
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-09-14 11:09:00 +0200

    Added pages

--- assets/html/head.js
+++ assets/html/head.js
@@ -3,17 +3,11 @@ document.write(`
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <link rel="stylesheet" href="assets/css/true.css"/>
-<link rel="stylesheet" href="assets/css/false.css"/>
 
-<link rel="apple-touch-icon" href="assets/user-interface/logo/dark/apple-touch-icon.png" sizes="180x180"/>
-<link rel="icon" href="assets/user-interface/logo/dark/favicon-32x32.png" type="image/png" sizes="32x32"/>
-<link rel="icon" href="assets/user-interface/logo/dark/favicon-16x16.png" type="image/png" sizes="16x16"/>
-<link rel="manifest" href="assets/user-interface/logo/dark/site.webmanifest"/>
-
-<meta name="twitter:card" content="summary"/>
-<meta name="twitter:title" content="HeadBodyScript"/>
-<meta name="twitter:description" content="Project-Prototype"/>
-<meta name="twitter:image" content="https://github.com/HeadBodyScript/HeadBodyScript/blob/main/user-interface/twetter-icon.png?raw=true"/>
+<link rel="apple-touch-icon" href="/assets/user-interface/logo/dark/apple-touch-icon.png" sizes="180x180"/>
+<link rel="icon" href="/assets/user-interface/logo/dark/favicon-32x32.png" type="image/png" sizes="32x32"/>
+<link rel="icon" href="/assets/user-interface/logo/dark/favicon-16x16.png" type="image/png" sizes="16x16"/>
+<link rel="manifest" href="/assets/user-interface/logo/dark/site.webmanifest"/>
 
 <title>HeadBodyScript</title>
 `);
--- assets/html/sidebar.js
+++ assets/html/sidebar.js
@@ -1,3 +1,27 @@
 document.write(`
-
+<div class="main">
+<section class="row" style="margin-top: 10px;">
+        <div class="card">
+            <picture>
+                <a class="main-logo" id="click" href="index.html">
+                <img src="/assets/user-interface/logo.png" style="width:auto;">
+                </a>
+            </picture>
+        </div>
+        <div class="card ref" id="hover-main">
+            <a href="/pages/about-me">About me</a>
+        </div>
+        <div class="card ref" id="hover-main">
+            <a href="/pages/hobbies">Hobbies</a>
+        </div>  
+        <div class="card ref" id="hover-main">
+            <a href="/pages/to-learn">To-learn:</a>
+        </div>
+        <div class="card lref">
+            <a>Tijl Pleuger</a></br>
+            <a>400927</a></br>
+            <a>Vista</a>
+        </div>
+</section>
+</div>
 `);
