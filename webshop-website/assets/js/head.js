89845e7c2136d146ca2283f257802965f8220a40
IHeadBodyScript <106414989+HeadBodyScript@users.noreply.github.com>
2023-09-19 23:23:00 +0200

    Jowza

---
+++ webshop-website/assets/js/head.js
@@ -0,0 +1,17 @@
+document.write(`
+<meta charset="UTF-8">
+<link rel="stylesheet" href="assets/css/init.css"/>
+<link rel="stylesheet" href="assets/css/dark.css" media="(prefers-color-scheme: dark)" />
+<link rel="stylesheet" href="assets/css/light.css" media="(prefers-color-scheme: light)" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="apple-touch-icon" href="assets/logo/dark/apple-touch-icon.png" media="(prefers-color-scheme: dark)" sizes="180x180"/>
+<link rel="icon" href="assets/logo/dark/favicon-32x32.png" type="image/png" media="(prefers-color-scheme: dark)" sizes="32x32"/>
+<link rel="icon" href="assets/logo/dark/favicon-16x16.png" type="image/png" media="(prefers-color-scheme: dark)" sizes="16x16"/>
+<link rel="manifest" href="assets/logo/dark/site.webmanifest" media="(prefers-color-scheme: dark)"/>
+
+<link rel="apple-touch-icon" href="assets/logo/light/apple-touch-icon.png" media="(prefers-color-scheme: light)" sizes="180x180"/>
+<link rel="icon" href="assets/logo/light/favicon-32x32.png" type="image/png" media="(prefers-color-scheme: light)" sizes="32x32"/>
+<link rel="icon" href="assets/logo/light/favicon-16x16.png" type="image/png" media="(prefers-color-scheme: light)" sizes="16x16"/>
+<link rel="manifest" href="assets/logo/light/site.webmanifest" media="(prefers-color-scheme: light)"/>
+`);
